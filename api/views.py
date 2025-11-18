ba643e3e05c9c9ee0e31690a874c1f254a498df5
Anis Jamil <anisjamiloffical1@gmail.com>
2025-11-18 15:13:20 +0500

    manual seriliazing data

--- api/views.py
+++ api/views.py
@@ -1,12 +1,9 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from students.models import Student
 # Create your views here.
 def StudentsView(request):
-    students = {
-        'id' : 1,
-        'name' : 'Anis jamil',
-        'age' : '20',
-        'skills' : ['python', 'django', 'rest framework']
-
-    }
-    return JsonResponse(students)
+    students = Student.objects.all()
+    student_list = list(students.values())
+      
+    return JsonResponse(student_list, safe=False)
--- students/models.py
+++ students/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Student(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    skills = models.TextField()
+
+    def __str__(self):
+        return self.name
